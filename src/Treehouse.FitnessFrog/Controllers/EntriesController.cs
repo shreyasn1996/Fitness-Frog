c5218a305c7862fbebf6d95c532f85410838652f
shreyas1196 <shreyas1196@outlook.com>
2017-10-29 16:07:00 +0530

    add post

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -41,6 +41,13 @@ namespace Treehouse.FitnessFrog.Controllers
 
         public ActionResult Add()
         {
+            
+            return View();
+        }
+        [ActionName("Add"),HttpPost]
+        public ActionResult AddPost()
+        {
+
             return View();
         }
 
